edb483d54ce183a0678b082c818a7946b251207d
phumudzo-nt <phumudzoyatuwani@gmail.com>
2024-10-08 10:18:22 +0200

    added html to javascript.

--- src/app.js
+++ src/app.js
@@ -53,6 +53,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+    <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☀</div>
+            <div class="weather-forecast-temperatures">
+              <strong>15°</strong>
+              <div class="weather-forecast-temperature">9°</div>
+            </div>
+            </div>
+          </div>
+        `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Johannesburg");
+
+displayForecast();
